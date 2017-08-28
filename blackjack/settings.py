4a5072ccb083e9bd2f58bcef10e7112fdbde1c70
HYXC <2393470186@qq.com>
2017-08-28 18:07:06 +0800

    Set django database

--- blackjack/settings.py
+++ blackjack/settings.py
@@ -73,13 +73,23 @@ WSGI_APPLICATION = 'blackjack.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blackjack',
+        'USER': 'ylhb',
+        'PASSWORD': '111abc',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
