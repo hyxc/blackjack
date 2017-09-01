b0fa39d94afb48beaaefedaf2d71b4c2a208bde8
HYXC <2393470186@qq.com>
2017-09-01 14:29:27 +0800

    Set TIME_ZONE

--- blackjack/settings.py
+++ blackjack/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -115,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
